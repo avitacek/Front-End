492fcf1f2ffd15364ff8880ed4d10a6ef92ae275
Andrew Vitacek <avitacek@gmail.com>
2017-08-23 16:41:10 -0500

    adding files

--- assets/tempotrip/js/src/global.js
+++ assets/tempotrip/js/src/global.js
@@ -31,15 +31,121 @@ function setTripsPage(){
      var table = $('#myTrips').DataTable({
     	"ajax": '../js/ajax/trips.json',
     	"select": true,
-    	"order": [[ 3, "desc" ]]
+    	"order": [[ 7, "desc" ]],
+        "columnDefs": [
+            {
+                "targets": [ 0 ],
+                "visible": true,
+            },
+            {
+                "targets": [ 1 ],
+                "visible": false,
+            },
+            {
+                "targets": [ 2 ],
+                "visible": false
+            },
+            {
+                "targets": [ 3 ],
+                "visible": true,
+            },
+            {
+                "targets": [ 4 ],
+                "visible": false
+            },
+            {
+                "targets": [ 5 ],
+                "visible": false
+            },
+            {
+                "targets": [ 6 ],
+                "visible": false
+            },
+            {
+                "targets": [ 7 ],
+                "visible": true,
+            },
+            {
+                "targets": [ 8 ],
+                "visible": true,
+            },
+            {
+                "targets": [ 9 ],
+                "visible": false
+            },
+            {
+                "targets": [ 10 ],
+                "visible": false,
+            },
+            {
+                "targets": [ 11 ],
+                "visible": true,
+            },
+            {
+                "targets": [ 12 ],
+                "visible": false
+            },
+            {
+                "targets": [ 13 ],
+                "visible": false
+            },
+            {
+                "targets": [ 14 ],
+                "visible": true,
+            },
+            {
+                "targets": [ 15 ],
+                "visible": false
+            },
+            {
+                "targets": [ 16 ],
+                "visible": false
+            },
+            {
+                "targets": [ 17 ],
+                "visible": false,
+            },
+            {
+                "targets": [ 18 ],
+                "visible": true,
+            },
+            {
+                "targets": [ 19 ],
+                "visible": true,
+            },
+            {
+                "targets": [ 20 ],
+                "visible": false
+            },
+            {
+                "targets": [ 21 ],
+                "visible": false
+            },
+            {
+                "targets": [ 22 ],
+                "visible": true
+            }
+        ]
     });
-    var tableRow = $('#myTrips tbody tr:first-child');
-
-     $('#myTrips tbody').on('click', 'tr', function () {
+    $('#myTrips tbody').on('click', 'tr', function () {
         var data = table.row( this ).data();
         $('#myTrips tbody tr').removeClass('selected');
         $(this).addClass('selected')
-        $('.deptCity').text(data[0]);
+        $('#deptHeader .City').text(data[0]);
+        $('#deptHeader .CityCountry').text(data[1]);
+        $('#arrvHeader .City').text(data[11]);
+        $('#arrvHeader .CityCountry').text(data[12]);
+
+        $('#departure-travel .departureDetails .airline').text(data[3]);
+        $('#departure-travel .departureDetails .flightNumber').text(data[4]);
+        $('#departure-travel .departureDetails .duration').text(data[5]);
+        $('#departure-travel .departureDetails .recordLocator').text(data[6]);
+
+        $('#departure-travel .departureTime .leave .date').text(data[7]);
+        $('#departure-travel .departureTime .leave .time').text(data[8]);
+        $('#departure-travel .departureTime .arrive .date').text(data[9]);
+        $('#departure-travel .departureTime .arrive .time').text(data[10]);
+
         $('.returnCity').text(data[1]);
         $('.tripType').text(data[2]);
         $('.deptDate').text(data[3]);
@@ -47,7 +153,10 @@ function setTripsPage(){
         $('.costs').text(data[5]);
         console.log(data);
     });
-   // $('#myTrips tbody tr:first-child').trigger('click');
+    setTimeout(function(){
+    	var tableRow = $('#myTrips tbody tr:first-child');
+    	$(tableRow).trigger('click');
+    },10);
 }
 $(document).ready(function(){
 	checkPage()
